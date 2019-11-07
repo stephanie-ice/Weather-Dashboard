028a3ed144a586801022762bd8bc4ffff8c950b3
Stephanie Ice <stephanie.icee@gmail.com>
2019-11-07 13:09:54 -0800

    finished .data JQ elements, and pseudocoded next API needs

--- logic.js
+++ logic.js
@@ -2,6 +2,8 @@ console.log ("The external JS is linked.")
 console.log ("The URL is "+ "http://api.openweathermap.org/data/2.5/weather?q=losangeles&appid=31e3da9a00a13c6735af3d6b9e899478")
 
 $(document).ready(function (){
+    /*Need API for Today's City, Temp, Icon, etc.*/
+    /*currently the URL is specific to Dallas and needs to incorporate a var*/
 $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=dallas&units=imperial&appid=31e3da9a00a13c6735af3d6b9e899478", 
     function (data) {
         console.log(data);
@@ -14,14 +16,28 @@ $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=dallas&units=imperia
         $(".city").append(city);
         console.log(city)
 
-        var temp = (data.main.temp);
+        var temp = (data.main.temp + " F");
         $(".temp").append(temp);
         console.log(temp);
 
-        var humidity = (data.main.humidity);
+        var humidity = (data.main.humidity + "%");
         $(".humidity").append(humidity);
         console.log(humidity);
+
+        var windSpeed = (data.wind.speed + " mph");
+        $(".windSpeed").append(windSpeed);
+        console.log(windSpeed);
     });
 
 
+    /*Need an API for the 5 Day forecast*/
+
+
+    /*Need an API for the UV INdex*/
+
+    /*Need an API for the Date*/
+
+    /*Need Search functionality, and search needs to feed the orginial URL for location*/
+
+    /*Need History functionality*/
 });

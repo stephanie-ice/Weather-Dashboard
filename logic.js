feb98cf3e8da67f03bcee6277b5648aa415ab0d8
Stephanie Ice <stephanie.icee@gmail.com>
2019-11-07 10:39:22 -0800

    ext.
    JQ CDN linked

--- logic.js
+++ logic.js
@@ -1,1 +1,10 @@
 console.log ("The external JS is linked.")
+console.log ("The URL is "+ "http://api.openweathermap.org/data/2.5/weather?q=losangeles&appid=31e3da9a00a13c6735af3d6b9e899478")
+
+$(document).ready(function (){
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=dallas&units=imperial&appid=31e3da9a00a13c6735af3d6b9e899478", 
+    function (data) {
+        console.log(data);
+});
+
+});

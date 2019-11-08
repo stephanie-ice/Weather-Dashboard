105bea6d6098eecf08e7cc1d7d1412f11b42f874
Stephanie Ice <stephanie.icee@gmail.com>
2019-11-08 14:22:40 -0800

    cleaned up 5Day code, created var for search place

--- logic.js
+++ logic.js
@@ -6,13 +6,29 @@ $(document).ready(function () {
     pull from that data instead of moment.js, which will produce the date of the
     location of the local computer, not the search location*/
         function today () {
-            var date = (moment().format("MMM/Do/YYYY"));
+            var date = (moment().format("MMM Do YYYY"));
             $(".date").append(date);
             console.log(date);
             };
         today ();
-    /*API for Today's City, Temp, Icon, etc.*/
-    /*currently the URL is specific to Dallas and needs to incorporate a var*/
+    /*Function for grabbing City from Search Bar*/
+    /*click event that responds when user enters text and clicks on search Btn*/
+        var place = $(".search").val()
+
+     /*   $("#submit").click(function () {
+            
+        });*/
+        console.log(place);
+    /*retrieves the value of the search input field*/
+    /*this become a var I can use for the API call*/
+    /*and store it in localstorage for use for the history function*/
+        /*localStorage.setItem("place",place)*/
+        
+
+    /*API for getting lon/lat of City*/
+
+
+    /*API for Today's City, Temp, Icon, Etc.*/
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=dallas&units=imperial&appid=31e3da9a00a13c6735af3d6b9e899478",
         function (data) {
             console.log(data);
@@ -51,11 +67,11 @@ $(document).ready(function () {
             console.log(day1icon);
 
             var day1temp = (forecastData.list[4].main.temp)
-            $(".day1temp").append(day1temp);
+            $(".day1temp").append("Temperature: " + day1temp + " F");
             console.log(day1temp);
 
             var day1humidity = (forecastData.list[4].main.humidity)
-            $(".day1humidity").append(day1humidity);
+            $(".day1humidity").append("Humidity: " + day1humidity + "%");
             console.log(day1humidity);
         };
         day1(forecastData)
@@ -64,17 +80,17 @@ $(document).ready(function () {
             var day2date = (forecastData.list[12].dt_txt)
             $(".day2date").append(day2date);
             console.log(day2date)
-
-            var day2temp = (forecastData.list[12].main.temp)
-            $(".day2temp").append(day2temp);
-            console.log(day2temp);
-
+            
             var day2icon = ("http://openweathermap.org/img/wn/" + forecastData.list[12].weather[0].icon + "@2x.png");
             $(".day2icon").attr("src", day2icon);
             console.log(day2icon);
 
+            var day2temp = (forecastData.list[12].main.temp)
+            $(".day2temp").append("Temperature: " + day2temp + " F");
+            console.log(day2temp);
+
             var day2humidity = (forecastData.list[12].main.humidity)
-            $(".day2humidity").append(day2humidity);
+            $(".day2humidity").append("Humidity: " + day2humidity + "%");
             console.log(day2humidity);
         };
         day2(forecastData)
@@ -83,17 +99,17 @@ $(document).ready(function () {
             var day3date = (forecastData.list[20].dt_txt)
             $(".day3date").append(day3date);
             console.log(day3date)
-
-            var day3temp = (forecastData.list[20].main.temp)
-            $(".day3temp").append(day3temp);
-            console.log(day3temp);
-
+            
             var day3icon = ("http://openweathermap.org/img/wn/" + forecastData.list[20].weather[0].icon + "@2x.png");
             $(".day3icon").attr("src", day3icon);
             console.log(day3icon);
 
+            var day3temp = (forecastData.list[20].main.temp)
+            $(".day3temp").append("Temperature: " + day3temp + " F");
+            console.log(day3temp);
+
             var day3humidity = (forecastData.list[20].main.humidity)
-            $(".day3humidity").append(day3humidity);
+            $(".day3humidity").append("Humidity: " + day3humidity + "%");
             console.log(day3humidity);
         };    
         day3(forecastData)
@@ -102,17 +118,17 @@ $(document).ready(function () {
             var day4date = (forecastData.list[28].dt_txt)
             $(".day4date").append(day4date);
             console.log(day4date)
-
-            var day4temp = (forecastData.list[28].main.temp)
-            $(".day4temp").append(day4temp);
-            console.log(day4temp);
-
+            
             var day4icon = ("http://openweathermap.org/img/wn/" + forecastData.list[28].weather[0].icon + "@2x.png");
             $(".day4icon").attr("src", day4icon);
             console.log(day4icon);
 
+            var day4temp = (forecastData.list[28].main.temp)
+            $(".day4temp").append("Temperature: " + day4temp + " F");
+            console.log(day4temp);
+
             var day4humidity = (forecastData.list[28].main.humidity)
-            $(".day4humidity").append(day4humidity);
+            $(".day4humidity").append("Humidity: " + day4humidity + "%");
             console.log(day4humidity);
         };    
         day4(forecastData)
@@ -121,17 +137,17 @@ $(document).ready(function () {
             var day5date = (forecastData.list[36].dt_txt)
             $(".day5date").append(day5date);
             console.log(day5date)
-
-            var day5temp = (forecastData.list[36].main.temp)
-            $(".day5temp").append(day5temp);
-            console.log(day5temp);
-
+            
             var day5icon = ("http://openweathermap.org/img/wn/" + forecastData.list[36].weather[0].icon + "@2x.png");
             $(".day5icon").attr("src", day5icon);
             console.log(day5icon);
 
+            var day5temp = (forecastData.list[36].main.temp)
+            $(".day5temp").append("Temperature: " + day5temp + " F");
+            console.log(day5temp);
+
             var day5humidity = (forecastData.list[36].main.humidity)
-            $(".day5humidity").append(day5humidity);
+            $(".day5humidity").append("Humidity: " + day5humidity + "%");
             console.log(day5humidity);
         };    
         day5(forecastData)
